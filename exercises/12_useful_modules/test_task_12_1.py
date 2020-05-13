8ead2dedebb36dad194f60c6aa131e2ce5b62398
pyneng <pyneng.course@gmail.com>
2020-05-13 06:36:22 +0000

    Chapter 12 updated

--- exercises/12_useful_modules/test_task_12_1.py
+++ exercises/12_useful_modules/test_task_12_1.py
@@ -6,8 +6,16 @@ sys.path.append("..")
 
 from common_functions import check_function_exists, ping, get_reach_unreach
 
+# Проверка что тест вызван через pytest ..., а не python ...
+from _pytest.assertion.rewrite import AssertionRewritingHook
+if not isinstance(__loader__, AssertionRewritingHook):
+    print(f"Тесты нужно вызывать используя такое выражение:\npytest {__file__}\n\n")
+
 
 def test_function_created():
+    """
+    Проверка, что функция создана
+    """
     check_function_exists(task_12_1, "ping_ip_addresses")
 
 
@@ -16,6 +24,9 @@ def test_function_created():
     reason="Этот тест работает только если создана функция ping_ip_addresses",
 )
 def test_function_return_value():
+    """
+    Проверка работы функции
+    """
     list_of_ips = ["1.1.1", "8.8.8.8", "8.8.4.4", "8.8.7.1"]
     correct_return_value = get_reach_unreach(list_of_ips)
 
