04158606856e33777dcc5e469883b546f047499c
pyneng <pyneng.course@gmail.com>
2020-05-18 08:23:31 +0000

    Chapter 26 tasks/tests renamed

--- exercises/26_oop_special_methods/test_task_26_1.py
+++ exercises/26_oop_special_methods/test_task_26_1.py
@@ -1,5 +1,5 @@
 import pytest
-import task_26_1
+import task_26_3
 import sys
 
 sys.path.append("..")
@@ -8,39 +8,20 @@ from common_functions import check_class_exists, check_attr_or_method
 
 
 def test_class_created():
-    check_class_exists(task_26_1, "Topology")
+    check_class_exists(task_26_3, "IPAddress")
 
 
-def test_attr_topology(topology_with_dupl_links):
-    """Проверяем, что в объекте Topology есть атрибут topology"""
-    top_with_data = task_26_1.Topology(topology_with_dupl_links)
-    check_attr_or_method(top_with_data, attr="topology")
+def test_attr_topology():
+    """Проверяем, что в объекте IPAddress есть атрибуты ip и mask"""
+    ip1 = task_26_3.IPAddress("10.1.1.1/24")
+    check_attr_or_method(ip1, attr="ip")
+    check_attr_or_method(ip1, attr="mask")
+    assert ip1.ip == "10.1.1.1", "Значение ip1.ip должно быть равным 10.1.1.1"
+    assert ip1.mask == 24, "Значение ip1.mask должно быть равным 24"
 
 
-def test_topology_normalization(topology_with_dupl_links, normalized_topology_example):
-    """Проверка удаления дублей в топологии"""
-    top_with_data = task_26_1.Topology(topology_with_dupl_links)
-    assert len(top_with_data.topology) == len(normalized_topology_example)
-
-
-def test_method__add__(normalized_topology_example):
-    """Проверка наличия метода __add__ и его работы"""
-    top1 = task_26_1.Topology(normalized_topology_example)
-    top1_size_before_add = len(top1.topology)
-    top2 = task_26_1.Topology(
-        {("R1", "Eth0/4"): ("R7", "Eth0/0"), ("R1", "Eth0/6"): ("R9", "Eth0/0")}
-    )
-    top2_size_before_add = len(top2.topology)
-
-    check_attr_or_method(top1, method="__add__")
-    top3 = top1 + top2
-    assert isinstance(
-        top3, task_26_1.Topology
-    ), "Метод __add__ должен возвращать новый экземпляр класса Topology"
-    assert len(top3.topology) == 8
-    assert (
-        len(top1.topology) == top1_size_before_add
-    ), "После сложения изменился размер первой топологии. Метод __add__ не должен менять исходные топологии"
-    assert (
-        len(top2.topology) == top2_size_before_add
-    ), "После сложения изменился размер второй топологии. Метод __add__ не должен менять исходные топологии"
+def test_wrong_ip():
+    with pytest.raises(ValueError) as excinfo:
+        ip1 = task_26_3.IPAddress("10.1.1.1/240")
+    with pytest.raises(ValueError) as excinfo:
+        ip1 = task_26_3.IPAddress("10.1.400.1/24")
