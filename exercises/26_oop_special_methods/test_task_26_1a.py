4ada0d6b145013b9ca8c59bd2a29a4cb059b7980
pyneng <pyneng.course@gmail.com>
2020-05-18 08:46:21 +0000

    Chapter 26 updated

--- exercises/26_oop_special_methods/test_task_26_1a.py
+++ exercises/26_oop_special_methods/test_task_26_1a.py
@@ -1,5 +1,5 @@
 import pytest
-import task_26_1
+import task_26_1a
 import sys
 
 sys.path.append("..")
@@ -13,20 +13,29 @@ if not isinstance(__loader__, AssertionRewritingHook):
 
 
 def test_class_created():
-    check_class_exists(task_26_1, "IPAddress")
+    check_class_exists(task_26_1a, "IPAddress")
 
 
 def test_attr_topology():
     """Проверяем, что в объекте IPAddress есть атрибуты ip и mask"""
-    ip1 = task_26_1.IPAddress("10.1.1.1/24")
+    ip1 = task_26_1a.IPAddress("10.1.1.1/24")
     check_attr_or_method(ip1, attr="ip")
     check_attr_or_method(ip1, attr="mask")
     assert ip1.ip == "10.1.1.1", "Значение ip1.ip должно быть равным 10.1.1.1"
     assert ip1.mask == 24, "Значение ip1.mask должно быть равным 24"
 
 
-def test_wrong_ip():
-    with pytest.raises(ValueError) as excinfo:
-        ip1 = task_26_1.IPAddress("10.1.1.1/240")
-    with pytest.raises(ValueError) as excinfo:
-        ip1 = task_26_1.IPAddress("10.1.400.1/24")
+def test_str_method():
+    """Проверяем __str__"""
+    ip1 = task_26_1a.IPAddress("10.5.5.5/24")
+    assert (
+        str(ip1) == "IP address 10.5.5.5/24"
+    ), "Метод __str__ должен возвращать 'IP address 10.5.5.5/24'"
+
+
+def test_repr_method():
+    """Проверяем __repr__"""
+    ip1 = task_26_1a.IPAddress("10.5.5.5/24")
+    assert (
+        repr(ip1) == "IPAddress('10.5.5.5/24')"
+    ), "Метод __repr__ должен возвращать IPAddress('10.5.5.5/24')"
--- exercises/26_oop_special_methods/test_task_26_2.py
+++ exercises/26_oop_special_methods/test_task_26_2.py
@@ -6,17 +6,25 @@ sys.path.append("..")
 
 from common_functions import check_class_exists, check_attr_or_method, strip_empty_lines
 
+# Проверка что тест вызван через pytest ..., а не python ...
+from _pytest.assertion.rewrite import AssertionRewritingHook
+if not isinstance(__loader__, AssertionRewritingHook):
+    print(f"Тесты нужно вызывать используя такое выражение:\npytest {__file__}\n\n")
+
 
 def test_class_created():
     check_class_exists(task_26_2, "CiscoTelnet")
 
 
-def test_method__enter__(first_router_from_devices_yaml):
-    r1 = task_26_2.CiscoTelnet(**first_router_from_devices_yaml)
+def test_method_enter_exit(first_router_from_devices_yaml):
     assert (
-        getattr(r1, "__enter__", None) != None
+        getattr(task_26_2.CiscoTelnet, "__enter__", None) != None
     ), "У класса CiscoTelnet должен быть метод __enter__"
 
     assert (
-        getattr(r1, "__exit__", None) != None
+        getattr(task_26_2.CiscoTelnet, "__exit__", None) != None
     ), "У класса CiscoTelnet должен быть метод __exit__"
+
+    with task_26_2.CiscoTelnet(**first_router_from_devices_yaml) as r1:
+        r1.send_show_command("sh clock")
+
