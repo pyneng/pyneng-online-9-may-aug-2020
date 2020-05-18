e952136182a3de523956d0cc1143fde19324da74
pyneng <pyneng.course@gmail.com>
2020-05-18 11:15:08 +0000

    Chapter 27 updated

--- exercises/27_oop_inheritance/task_27_1a.py
+++ exercises/27_oop_inheritance/task_27_1a.py
@@ -19,8 +19,8 @@ In [2]: device_params = {
 
 In [3]: r1 = CiscoSSH(**device_params)
 Введите имя пользователя: cisco
-Введите пароль:
-Введите пароль для режима enable:
+Введите пароль: cisco
+Введите пароль для режима enable: cisco
 
 In [4]: r1.send_show_command('sh ip int br')
 Out[4]: 'Interface                  IP-Address      OK? Method Status                Protocol\nEthernet0/0                192.168.100.1   YES NVRAM  up                    up      \nEthernet0/1                192.168.200.1   YES NVRAM  up                    up      \nEthernet0/2                190.16.200.1    YES NVRAM  up                    up      \nEthernet0/3                192.168.230.1   YES NVRAM  up                    up      \nEthernet0/3.100            10.100.0.1      YES NVRAM  up                    up      \nEthernet0/3.200            10.200.0.1      YES NVRAM  up                    up      \nEthernet0/3.300            10.30.0.1       YES NVRAM  up                    up      '
@@ -28,3 +28,4 @@ Out[4]: 'Interface                  IP-Address      OK? Method Status           
 """
 
 device_params = {"device_type": "cisco_ios", "ip": "192.168.100.1"}
+
