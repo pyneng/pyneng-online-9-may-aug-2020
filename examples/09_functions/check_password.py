2ac059e77f478fa98febfc4363f9f586200f2141
pyneng <pyneng.course@gmail.com>
2020-06-15 08:36:40 +0000

    lecture 9

--- examples/09_functions/check_password.py
+++ examples/09_functions/check_password.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 from pprint import pprint
 
-def check_passwd(username, password, min_passwd_len=8, spec_sym=None):
-    #print(f"PARAMS: {username:10} {password:20} {min_passwd_len}")
+
+def check_passwd(
+    username, password, min_passwd_len=8, spec_sym=None, numbers=False
+):
     if len(password) < min_passwd_len:
         message = "Пароль слишком короткий"
         return False, message
     elif username.lower() in password.lower():
         message = "Пароль содержит имя пользователя"
+        return False, message
+    elif numbers and not set("0123456789") & set(password):
+        message = f"Пароль должен содержать хотя бы одно число"
         return False, message
     elif spec_sym and not set(spec_sym) & set(password):
         message = f"Пароль должен содержать один из символов {spec_sym}"
@@ -17,13 +22,15 @@ def check_passwd(username, password, min_passwd_len=8, spec_sym=None):
         return True, message
 
 
-def select_correct_passwd(check_data):
+# ключевые аргументы переменной длины
+def select_correct_passwd(check_data, **kwargs):
     correct_password = []
     incorrect_password = []
 
     for data in check_data:
         user, passwd = data
-        check, message = check_passwd(user, password=passwd, spec_sym="@$#!")
+        # распаковка словаря в ключевые аргументы
+        check, message = check_passwd(user, password=passwd, **kwargs)
         if check:
             correct_password.append([user, passwd])
         else:
@@ -31,11 +38,11 @@ def select_correct_passwd(check_data):
     return correct_password, incorrect_password
 
 
-data= [
+data = [
     ["user10", "sdldfj"],
     ["user20", "sdf####klfdj"],
     ["user30", "ssdkfjsus#%er3df"],
 ]
-yes, no = select_correct_passwd(data)
+yes, no = select_correct_passwd(data, spec_sym="@#$", numbers=True)
 pprint(yes)
 pprint(no)
